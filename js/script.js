8970fa9e47d2168772ef622fc21b0005882b88be
coldwhiskeyman <eldiablo5783@gmail.com>
2020-05-22 10:40:16 +0300

    smooth transitions, css, js, img minified

--- js/script.js
+++ js/script.js
@@ -28,16 +28,59 @@ $(function () {
     },
   });
 
+  popup = $('.popup-container');
+  menu = $('.menu')
+
   $('.show-menu-btn, .menu__close-btn, .menu__link').click(function() {
-    $('.menu').toggle();
+    if (menu.hasClass('closed')) {
+      menu.removeClass('closed');
+      setTimeout(function(){
+        menu.removeClass('hidden');
+      }, 10);
+    } else {
+      menu.addClass('hidden');
+      setTimeout(function(){
+        menu.addClass('closed');
+      }, 500);
+    }
   });
 
   $('.callback-btn, .callback-btn-full, .order-btn, .contact-btn').click(function () {
-    $('.popup-container').show();
+    popup.removeClass('closed');
+    setTimeout(function(){
+      popup.removeClass('hidden');
+    }, 10);
   });
-  $('.popup-container').click(function (event) {
+  popup.click(function (event) {
     if (event.target === this) {
-      $(this).hide();
-    }
+      $(this).addClass('hidden');
+      setTimeout(function(){
+        popup.addClass('closed');
+      }, 500);
+    };
+  });
+  $('.leave-contacts__close').click(function() {
+    popup.addClass('hidden');
+    setTimeout(function(){
+      popup.addClass('closed');
+    }, 500);
+  })
+
+  $('a[href^="#"]').on('click', function(event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+    
+    var sc = $(this).attr("href"),
+        dn = $(sc).offset().top;
+    /*
+    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+    * dn - определяем положение блока на странице
+    */
+    
+    $('html, body').animate({scrollTop: dn}, 500);
+    
+    /*
+    * 500 скорость перехода в миллисекундах
+    */
   });
 });
